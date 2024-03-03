e620e2b21456c8686b8b7ae24a298d184479a4f8
Denis <denis.yaremenko@huma.com>
2024-03-03 15:18:31 +0200

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Denis Yaremenko on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
